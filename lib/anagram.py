762016ebb5843d057334df5c0341065b1ec07fea
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-10 16:54:26 -0400

    tough love on anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+
+class Anagram:
+   def __init__(self, word):
+      self.word_sorted = sorted([letter for letter in word])
+   
+   def match(self, check_list):
+      match_check_list = []
+
+      for item in check_list:
+         if sorted([letter for letter in item]) == self.word_sorted:
+            match_check_list.append(item)
+         
+      return match_check_list
+   
+   
+
+
+
